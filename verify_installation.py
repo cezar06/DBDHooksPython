5143ee94f82cd44b9323038a4b05c45435312189
cezar06 <@tudor.cezar01@gmail.com>
2024-10-05 17:10:57 +0300

    Initial commit: Set up development environment

---
+++ verify_installation.py
@@ -0,0 +1,10 @@
+import cv2
+from PIL import Image
+import pyautogui
+import tkinter as tk
+
+def main():
+    print("All packages are installed correctly!")
+
+if __name__ == "__main__":
+    main()
